07ce62691c552a4c781631a5b4eca0cc550200ea
GiorgioDordo <djole6789@gmail.com>
2024-12-29 20:23:45 +0100

    refactor logic error 404

--- functions.php
+++ functions.php
@@ -23,4 +23,12 @@ function urlIs($value) {
     return $_SERVER['REQUEST_URI'] === $value;
 }
 
+function abort() {
+    http_response_code(404);
+    
+    require 'views/404.view.php';
+    
+    die();
+}
+
 ?>
--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path']; //aggiungendo path alla fine, faccio in modo che quando vado a fare dd() di uri, mi restituisca solo il path della pagina
+//Todo: aggiungendo path alla fine, faccio in modo che quando vado a fare dd() di uri, mi restituisca solo il path della pagina
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 // dd($uri);
 
 // Todo: separate the path from the query string
@@ -16,8 +17,6 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path']; //aggiungendo path alla fine,
 //     require 'controllers/contact.php';
 // }
 
-
-
 // reformat del codice precedente
 $routes = [
     '/' => 'controllers/index.php',
@@ -29,7 +28,5 @@ if (array_key_exists($uri, $routes)) {
     require $routes[$uri];
 } else {
     // se il link non viene trovato allora restituire l'errore 404
-    require 'views/404.view.php';
-    
-    die();
+    abort();
 }
