fbc5c73df70e3e60569fa4a715030f1560778b28
GiorgioDordo <djole6789@gmail.com>
2025-01-05 21:41:17 +0100

    refactor Database class to accept configuration parameters and create config.php for database settings

--- Database.php
+++ Database.php
@@ -1,19 +1,24 @@
 <?php
 //Todo: connessione al MySql database
 class Database {
-public $connection;
+    public $connection;
 
-public function __construct() {
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=root;charset=utf8mb4"; // it declares how characters are encoded if we are not working with english or something similiar
+    public function __construct($config, $username = 'root', $password = 'root') {
 
-$this->connection = new PDO($dsn); //data source name, a string that declares your connection to the database
+        $dsn = 'mysql:' . http_build_query($config, '', ';'); // it declares how characters are encoded if we are not working with english or something similiar
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //fetch all the results as opposed to a single result or a single record and give me the results as an associative array
+        ]); //data source name, a string that declares your connection to the database
     }
 
-public function query($query) {
-$statement = $this->connection->prepare($query); //preparing this query to send to mysql where it will be executed
+        
+    public function query($query) {
+        
+        $statement = $this->connection->prepare($query); //preparing this query to send to mysql where it will be executed
 
-$statement->execute();
+        $statement->execute();
 
-return $statement; //fetch all the results as opposed to a single result or a single record and give me the results as an associative array
+        return $statement; 
     }
 }
--- index.php
+++ index.php
@@ -4,7 +4,10 @@ require 'functions.php';
 require 'Database.php';
 // require 'router.php';
 
-$db = new Database();
+$config = require('config.php');
+
+$db = new Database($config[ 'database' ]);
+
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 // dd($posts);
 
@@ -12,7 +15,7 @@ foreach ($posts as $post) {
     echo "<li>" . $post["title"] . "</li>";
 }
 
-// Todo: classes, a blueprint of what a person is and what he can do
+// Todo: classes, a blueprint of what a person is and what he can do (properties and methods), a class is a blueprint of an object and an object is an instance of a class 
 // class Person {
 //     public $name;
 //     public $age;
